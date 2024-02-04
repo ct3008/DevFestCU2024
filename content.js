8d802b49c659123bd65d1030dd1f4f852f3bc91f
susannaha8 <66691218+susannaha8@users.noreply.github.com>
2024-02-04 04:13:32 -0500

    commented out messages between extension and content-script

--- content.js
+++ content.js
@@ -6,8 +6,8 @@ catImage.style.position = 'fixed';
 catImage.style.top = '10%';
 catImage.style.left = '90%';
 catImage.style.transform = 'translate(-50%, -50%)';
-catImage.style.maxWidth = '30%'; // Ensure the image doesn't exceed the viewport width
-catImage.style.maxHeight = '30%'; // Ensure the image doesn't exceed the viewport height
+catImage.style.maxWidth = '100px'; // Ensure the image doesn't exceed the viewport width
+catImage.style.maxHeight = '100px'; // Ensure the image doesn't exceed the viewport height
 catImage.style.zIndex = '9999';
 
 document.body.append(catImage);
@@ -29,6 +29,7 @@ catImage.onmousedown = function(event) {
 	}
   
 	function onMouseMove(event) {
+        console.log("MOUSE EVENT");
 	  moveAt(event.pageX, event.pageY);
 	}
   
@@ -46,20 +47,102 @@ catImage.ondragstart = function() {
 	return false;
 }
 
+// async function test() {
+//     try {
+//         const response = await chrome.runtime.sendMessage({greeting: "hello"});
+//         console.log(response);
+//     }
+//     catch (error){
+//         console.log(error);
+//     }
+// }
+
+// test();
+
+
+//based off of https://medium.com/backticks-tildes/how-i-developed-apples-screen-time-chrome-extension-988e0c451894
+
+let tab = null;
+let current_host = "";
+let current_total = 0;
+let current_start = 0;
+
+//update total time in local storage
+async function end() {
+
+    //calculate new total time to include finished session
+    let total_time = Date.now() - current_start + current_total
+    console.log("total time:");
+    console.log(total_time);
+    try {
+        await chrome.storage.local.set({current_host: total_time});
+    } catch(error) {
+        console.log(error);
+    }  
+}
+
+//switch current tab info
+async function setActive() {
+    console.log("set active. current host:");
+    console.log(current_host);
+    
+    //if tabs have changed,
+    if (current_host != tab) {
+        console.log("tabs have changed");
+
+        //end old host
+        end();
+
+        //set new host, start, and total
+        current_host = tab;
+        current_start = Date.now();
+        try {
+            current_total = await chrome.storage.local.get([current_host]);
+            console.log("current total from get:")
+            console.log(result);
+        } catch(error) {
+            current_total = 0;
+            console.log(error);
+        }
+    }  
+}
+
+window.addEventListener("visibilitychange", function() {
+    console.log("VISIBILITY");
+    tab = window.location.href;
+    console.log(tab);
+    setActive();
+});
+
+//when tabs are changed
+// document.addEventListener("visibilitychange", function () {
+//     // setActive();
+// });
+
+
+
 // recieve a message:
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting === "hello") {
-        if (catImage.style.display == "none"){
-            catImage.style.display = "block";
-        }
-        else {
-            catImage.style.display = "none";
-        }
-        sendResponse({farewell: "goodbye"});
-      }
-    }
-  );
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//       console.log(sender.tab ?
+//                   "from a content script:" + sender.tab.url :
+//                   "from the extension");
+//       if (request.greeting === "hello") {
+//         console.log("Hello greeting");
+//         if (catImage.style.display == "none"){
+//             catImage.style.display = "block";
+//         }
+//         else {
+//             catImage.style.display = "none";
+//         }
+//         sendResponse({farewell: "goodbye"});
+//       }
+//       if (request.greeting === "tab") {
+//         console.log("TAB");
+//         tab = request;
+//         //setActive();
+//         sendResponse({farewell: "tab recieved"});
+//       }
+//     }
+//   );
+
--- popup.js
+++ popup.js
@@ -2,12 +2,72 @@
 const button = document.getElementById("mybutton");
 // // const cat = document.getElementById("cat")
 
-button.onclick = function() {
-	//send a message:
-	(async () => {
-		const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-		const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
-		// do something with response here, not outside the function
-		console.log(response);
-	})();
-}
+//turning the cat on and off
+// button.onclick = function() {
+// 	//send a message:
+// 	(async () => {
+// 		const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+// 		const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
+// 		// do something with response here, not outside the function
+// 		console.log(response);
+// 	})();
+// }
+
+
+// chrome.storage.local.get(["key"]).then((result) => {
+// 	console.log("Value is " + result.key);
+// });
+
+// document.addEventListener("visibilitychange", function () {
+	
+// 	console.log(Date.now());
+// 	//set and get storage
+// 	chrome.storage.local.set({ "start_time": "string" }).then(() => {
+// 		console.log("Value is set");
+// 	});
+// 	chrome.storage.local.get(["start_time"]).then((result) => {
+// 	console.log("Value is " + result.key);
+// 	});
+// 	console.log(Date.now());
+// });
+
+// document.addEventListener("visibilitychange", function () {
+
+// 	(async () => {
+// 		console.log(Date.now());
+// 		const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+// 		// console.log(tab.id);
+// 		// console.log(tab.url);
+// 		// urlObj = new URL(url);
+// 		console.log(Date.now());
+// 		const response = await chrome.tabs.sendMessage(tab.id, {greeting: "tab"});
+// 		console.log(response);
+// 	});
+// });
+
+// async function test() {
+// 	console.log(Date.now());
+// 	const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+// 	// console.log(tab.id);
+// 	// console.log(tab.url);
+// 	// urlObj = new URL(url);
+// 	console.log(Date.now());
+// 	const response = await chrome.tabs.sendMessage(tab.id, {greeting: "tab"});
+// 	console.log(response);
+// }
+
+// test();
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+	  console.log(sender.tab ?
+		      "from a content script:" + sender.tab.url :
+		      "from the extension");
+	  if (request.greeting === "hello")
+	    sendResponse({farewell: "gooooodbye"});
+	}
+);
+
+// document.addEventListener("visibilitychange", function () {
+// 	console.log("EVENT LISTENER");
+// });
